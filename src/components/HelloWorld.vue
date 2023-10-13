5c04b74ab0a4925aaf8bf56d264d7be384419cd9
PikiTv <soellnerphilipp@web.de>
2023-10-13 17:12:33 +0200

    bilder hinzugefügt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
+  <h1>Barcode Scanner</h1>
   <div>
     <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" class="camera-container"></StreamBarcodeReader>
     <p v-if="scannedBarcode">Gescannter Barcode: {{ scannedBarcode }}</p>
+    <img v-if="scannedBarcode && images[scannedBarcode]" :src="images[scannedBarcode]" alt="Scanned Product Image" />
   </div>
 </template>
 
@@ -14,7 +16,13 @@ export default {
   },
   data() {
     return {
-      scannedBarcode: null
+      scannedBarcode: null,
+      product: ["5060896625829", "90357350", "5000159472029"],
+      images: {
+        "5060896625829": "https://images.gopuff.com/blob/gopuffcatalogstorageprod/catalog-images-container/resize/cf/version=1_0,format=auto,fit=scale-down,width=800,height=800/dbbe25d8-813d-4743-a256-1e7d4f1b0fae.png",
+        "90357350": "https://img.rewe-static.de/6652816/4778180_digital-image.png",
+        "5000159472029": "https://www.scheiding-online.de/product/image/medium/613014_m%26m-crispy-213g.png",
+      }
     };
   },
   methods: {
@@ -36,5 +44,9 @@ export default {
   .camera-container {
     transform: scaleX(-1); /* Spiegele die Kameraanzeige horizontal */
   }
+}
+img{
+  max-width: 300px;
+  max-height: 300px;
 }
 </style>
