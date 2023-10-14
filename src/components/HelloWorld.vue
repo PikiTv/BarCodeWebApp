ac74addd5a00c0f2116d43367dc73a8d8bee0470
PikiTv <soellnerphilipp@web.de>
2023-10-14 13:57:40 +0200

    beendet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Barcode Scanner</h1>
   <div>
-    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" class="camera-container"></StreamBarcodeReader>
-    <p v-if="scannedBarcode">Gescannter Barcode: {{ scannedBarcode }}</p>
-    <img v-if="scannedBarcode && images[scannedBarcode]" :src="images[scannedBarcode]" alt="Scanned Product Image" />
+    <h1 class="title">Barcode Scanner</h1>
+    <div>
+      <button @click="toggleCamera" class="scan-button">
+        {{ isCameraOn ? 'Kamera ausschalten' : 'Hier scannen' }}
+      </button>
+      <StreamBarcodeReader
+        ref="barcodeReader"
+        @decode="onDecode"
+        @loaded="onLoaded"
+        class="camera-container"
+        v-if="isCameraOn"
+      ></StreamBarcodeReader>
+      <p v-if="scannedBarcode" class="barcode-text">
+        Gescannter Barcode: {{ scannedBarcode }}
+      </p>
+      <img
+        v-if="scannedBarcode && images[scannedBarcode]"
+        :src="images[scannedBarcode]"
+        alt="Scanned Product Image"
+        class="product-image"
+      />
+      <div v-if="savedDataArray.length > 0">
+        <h2 class="saved-barcodes-title">Gespeicherte Barcodes aus dem Backend:</h2>
+        <div class="saved-barcode" v-for="(item, index) in savedDataArray" :key="index">
+          {{ item }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,17 +41,18 @@ export default {
   },
   data() {
     return {
+      isCameraOn: false,
       scannedBarcode: null,
-      product: ["5060896625829", "90357350", "5000159472029"],
       images: {
         "5060896625829": "https://images.gopuff.com/blob/gopuffcatalogstorageprod/catalog-images-container/resize/cf/version=1_0,format=auto,fit=scale-down,width=800,height=800/dbbe25d8-813d-4743-a256-1e7d4f1b0fae.png",
         "90357350": "https://img.rewe-static.de/6652816/4778180_digital-image.png",
-        "5000159472029": "https://www.scheiding-online.de/product/image/medium/613014_m%26m-crispy-213g.png",
-      }
+        "5000159472029": "https://www.scheiding-online.de/product/image/medium/613014_m%26m-crispy-213g.png"
+      },
+      savedDataArray: []
     };
   },
   created() {
-    this.fetchScannedBarcode();
+    this.fetchSavedDataArray();
   },
   methods: {
     onDecode(decodedData) {
@@ -37,36 +62,77 @@ export default {
     onLoaded() {
       console.log("Kamera ist geladen.");
     },
-    async fetchScannedBarcode() {
-      try {
-        const response = await axios.get("http://localhost:8080/product"); // Verändert, um zum Backend-Endpoint zu passen
-        if (response.data && response.data.scannedBarcode) {
-          this.scannedBarcode = response.data.scannedBarcode;
-        }
-      } catch (error) {
-        console.error("Fehler beim Abrufen des gescannten Barcodes:", error);
-      }
-    },
     async saveScannedBarcode(barcode) {
       try {
-        await axios.post("http://localhost:8080/product", { scannedBarcode: barcode }); // Verändert, um zum Backend-Endpoint zu passen
+        await axios.post("http://localhost:8080/product", { barcode });
         console.log("Gespeicherter Barcode:", barcode);
+        this.savedDataArray.push(barcode);
       } catch (error) {
         console.error("Fehler beim Speichern des gescannten Barcodes:", error);
       }
     },
+    async fetchSavedDataArray() {
+      try {
+        const response = await axios.get("http://localhost:8080/product");
+        this.savedDataArray = response.data;
+      } catch (error) {
+        console.error("Fehler beim Abrufen des Arrays aus dem Backend:", error);
+      }
+    },
+    toggleCamera() {
+      this.isCameraOn = !this.isCameraOn;
+      if (!this.isCameraOn) {
+        // Hier kannst du die Logik zum Ausschalten der Kamera einfügen, falls erforderlich.
+      }
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
 @media (min-width: 1500px) {
   .camera-container {
     transform: scaleX(-1);
   }
 }
-img {
+.title {
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.scan-button {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.scan-button:hover {
+  background-color: #0056b3;
+}
+
+.barcode-text {
+  font-size: 18px;
+  margin: 20px 0;
+}
+
+.product-image {
   max-width: 300px;
   max-height: 300px;
+  display: block;
+  margin: 10px auto;
+}
+
+.saved-barcodes-title {
+  font-size: 20px;
+  margin: 20px 0;
+}
+
+.saved-barcode {
+  font-size: 16px;
+  margin: 5px 0;
 }
 </style>
